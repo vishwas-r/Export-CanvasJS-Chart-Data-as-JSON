974b0b136f31c25b6272e3eec21723fb2fd45cd8
Vishwas R <vishwas.r@fenopix.com>
2026-02-12 11:21:51 +0530

    Bug Fix: Position of export button was not proper since the recent changes in CanvasJS library

--- src/CanvasJS Data As JSON.js
+++ src/CanvasJS Data As JSON.js
@@ -1,54 +1,50 @@
 (function () {
     var CanvasJS = window.CanvasJS || CanvasJS ? window.CanvasJS : null;
+
+    if (CanvasJS == null)
+        CanvasJS = require("@canvasjs/charts")
+
     if (CanvasJS) {
         CanvasJS.Chart.prototype.exportAsJSON = function (fileName) {
             CanvasJSDataAsJSON(this, fileName);
         }
+
+        var chartRender = CanvasJS.Chart.prototype.render;
+        CanvasJS.Chart.prototype.render = function (options) {
+            var result = chartRender.apply(this, arguments);
+            this.exportAsJSON();
+            return result;
+        }
     }
 
     function CanvasJSDataAsJSON(chart, fileName) {
-        if (chart.options.exportEnabled) {
-            var exportJSON = document.createElement('div');
-            var text = document.createTextNode("Save as JSON");
-            exportJSON.setAttribute("style", "padding: 12px 8px; background-color: " + chart.toolbar.itemBackgroundColor + "; color: " + chart.toolbar.fontColor);
-            exportJSON.appendChild(text);
-            exportJSON.addEventListener("mouseover", function () {
-                exportJSON.setAttribute("style", "padding: 12px 8px; background-color: " + chart.toolbar.itemBackgroundColorOnHover + "; color: " + chart.toolbar.fontColorOnHover);
-            });
-            exportJSON.addEventListener("mouseout", function () {
-                exportJSON.setAttribute("style", "padding: 12px 8px; background-color: " + chart.toolbar.itemBackgroundColor + "; color: " + chart.toolbar.fontColor);
-            });
-            exportJSON.addEventListener("click", function () {
-                parseJSON({
-                    filename: (fileName || "chart-data") + ".json",
-                    chart: chart
-                })
-            });
+        if (chart.exportEnabled && chart._dropdownMenu) {
+            var exportJSON = chart._dropdownMenu.getElementsByClassName("canvasjs-chart-json-export")[0];
 
-            chart._toolBar.lastChild.appendChild(exportJSON);
-        } else {
-            var base64Img = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAACXBIWXMAAA7DAAAOwwHHb6hkAAAAB3RJTUUH4wISBxkv6UqNPAAAABN0RVh0U29mdHdhcmUAUGhvdG9TY2FwZYB1kb8AAAFMSURBVDhP3dM7LwVBGMbxiUvE/VpqJKIQhdAplKg4CQoF30JCpdAqSHwBLaFW6IWGIBKFOAp3CSHRkPB/ZncnY8+cwxEaT/LLzszOO3ubNf82ZRiMmr+XY6yiwvYCaUd3Sh864acBXTjEO7bRBhddYQWv0ARfFr1QhrCBW6TnXWMANpqUniCbqEcl9GihOWnTcJ0HTGIcw1CacAS/qJAXuM6FOl5KsAO/oJAD6N26gStUayDOLPwC0R2cYh/P8ZisoRY2yaC/YB3ukZzTex5FK7QHlT3o3JzteQktmIHGdqGtE8oWpqKmaUFV1AwvuIx1lNtebkrRGDXNCE7QbHsktGBPfPwqE1DtE3Lu8BJ5f6M8WUJS6z5osqDuUL+VHvO7FqDaG9TA5g4afMM5zorwCNVqD+sCNv3wt8hPzOBTOjAPfd1iLGIMfxVjPgAX3Z+XV+s1aQAAAABJRU5ErkJggg==";
-            var exportButton = document.createElement('button');
-            var chartBound = chart.container.getBoundingClientRect();
+            if (!exportJSON) {
+                exportJSON = document.createElement('div');
+                exportJSON.setAttribute("class", "canvasjs-chart-json-export");
+                var text = document.createTextNode("Save as JSON");
+                exportJSON.setAttribute("style", "padding: 12px 8px; cursor: pointer; background-color: " + chart.toolbar.itemBackgroundColor + "; color: " + chart.toolbar.fontColor);
+                exportJSON.appendChild(text);
+                exportJSON.addEventListener("mouseover", function () {
+                    exportJSON.setAttribute("style", "padding: 12px 8px; cursor: pointer; background-color: " + chart.toolbar.itemBackgroundColorOnHover + "; color: " + chart.toolbar.fontColorOnHover);
+                });
+                exportJSON.addEventListener("mouseout", function () {
+                    exportJSON.setAttribute("style", "padding: 12px 8px; cursor: pointer; background-color: " + chart.toolbar.itemBackgroundColor + "; color: " + chart.toolbar.fontColor);
+                });
+                exportJSON.addEventListener("click", function () {
+                    parseJSON({
+                        filename: (fileName || "chart-data") + ".json",
+                        chart: chart
+                    })
+                });
 
-            exportButton.style.cssText = "position:relative;display: inline-block;padding: 0px 4px;width: 35px;height: 27px;cursor: pointer;text-align: center;text-decoration: none;background-color: " + chart.toolbar.itemBackgroundColor + ";border: 1px solid " + chart.toolbar.borderColor + ";left:" + (chart.container.clientWidth - (chart.options.zoomEnabled ? 115 : 60)) + "px; top: 1px";
-            var img = document.createElement("IMG");
-            img.setAttribute("src", base64Img);
-            exportButton.appendChild(img);
-            exportButton.addEventListener("mouseover", function () {
-                this.style.cssText = this.style.cssText + "background-color: " + chart.toolbar.itemBackgroundColorOnHover;
-            });
-            exportButton.addEventListener("mouseout", function () {
-                this.style.cssText = this.style.cssText + "background-color: " + chart.toolbar.itemBackgroundColor;
-            });
-            exportButton.addEventListener("click", function () {
-                parseJSON({
-                    filename: (fileName || "chart-data") + ".json",
-                    chart: chart
-                })
-            });
+                chart._dropdownMenu.appendChild(exportJSON);
+            }
 
-            chart.container.appendChild(exportButton);
+            if (exportJSON.parentNode === chart._dropdownMenu)
+                chart._dropdownMenu.appendChild(exportJSON);
         }
     };
 
